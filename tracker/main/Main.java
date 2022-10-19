3c6f1b14489a48f7b9e4f7bfa689a4c9d1306930
Nikola Tapandželić <109683915+Nikola1998@users.noreply.github.com>
2022-10-19 21:56:25 +0200

    Delete tracker directory

--- tracker/main/Main.java
+++
@@ -1,8 +0,0 @@
-package tracker.main;
-
-public class Main {
-    public static void main(String[] args) {
-        CommandLine commandLine = new CommandLine();
-        commandLine.startProgram();
-    }
-}
